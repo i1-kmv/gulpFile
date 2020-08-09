bc7eee197f53545eeaa6e1a7237ab01e4a4560bd
i1-kmv <i1-kmv@mail.ru>
2020-08-09 10:59:31 +0200

    Настроил работу с js файлами и изображениями

--- gulpfile.js
+++ gulpfile.js
@@ -35,8 +35,14 @@ let { src, dest} = require('gulp'),
     autoprefixer = require("gulp-autoprefixer"),
     group_media = require("gulp-group-css-media-queries"),
     clean_css = require("gulp-clean-css"),
-    rename = require("gulp-rename")
-
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify-es").default,
+    imagemin = require("gulp-imagemin"),
+    webp = require("gulp-webp"),
+    webphtml = require("gulp-webp-html"),
+    webpcss = require("gulp-webpcss"),
+    svgSprite = require("gulp-svg-sprite")
+   
 
 
 function browserSync(params) {
@@ -52,6 +58,7 @@ function browserSync(params) {
 function html() {
   return src(path.src.html)
     .pipe(fileinclude())
+    .pipe(webphtml())
     .pipe(dest(path.build.html))
     .pipe(browsersync.stream())
 }
@@ -70,6 +77,11 @@ function css() {
         cascade: true
       })
     )
+    .pipe(
+      webpcss({
+        webpClass: '.webp',noWebpClass: '.no-webp'
+      })
+    )  
     .pipe(dest(path.build.css))
     .pipe(clean_css())
     .pipe(
@@ -81,16 +93,68 @@ function css() {
     .pipe(browsersync.stream())
 }
 
+function js() {
+  return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(
+      uglify()
+    )
+    .pipe(
+      rename({
+        extname: ".min.js"
+      })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
+}
+
+function images() {
+  return src(path.src.img)
+    .pipe(
+      webp({
+        quality: 70
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))  
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{ removeViewBox: false}],
+        interlaced: true,
+        optimizationLevel: 3
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
+
+gulp.task('svgSprite', function() {
+  return gulp.src([source_folder + '/iconsprite/*.svg'])
+  .pipe(svgSprite({
+    mode: {
+      stack: {
+        sprite: "../icons/icons.svg"
+      
+      }
+    }
+  }))
+  .pipe(dest(patch.build.img))
+})
+
 function watchFiles(params) {
   gulp.watch([path.watch.html], html)
   gulp.watch([path.watch.css], css)
+  gulp.watch([path.watch.js], js)
+  gulp.watch([path.watch.img], images)
 }
 
 function clean(params) {
   return del(path.clean)
 }
 
-let build = gulp.series(clean, gulp.parallel(css, html))
+let build = gulp.series(clean, gulp.parallel(js, css, html, images))
 let watch = gulp.parallel(build, watchFiles, browserSync)
 
 
@@ -99,3 +163,5 @@ exports.watch = watch
 exports.default = watch
 exports.html = html
 exports.css = css
+exports.js = js
+exports.images = images
